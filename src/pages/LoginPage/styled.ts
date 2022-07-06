26c2939d427e4b4027f69444d382054e0101b832
DanielRogs <danielrocha.dr142@gmail.com>
2022-07-06 13:41:00 -0300

    Início da mudança ao Styled Components

---
+++ src/pages/LoginPage/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    height: 10vh;
+    margin-bottom: 20vh;
+
+    .scheduleTitle{
+        
+    }
+`;
