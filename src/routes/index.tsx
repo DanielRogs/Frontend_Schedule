72f07e53f2d08143400e103d55d1f09c88597cfc
Gabriel <gabriel.costa513@gmail.com>
2022-07-07 12:17:51 -0300

    adicionando rotas básicas

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Route, Routes } from 'react-router-dom';
+import LoginPage from "../pages/LoginPage";
+import CadPage from "../pages/Cad_Page";
+
+export default class AppRoutes extends React.Component{
+
+    
+    userNotLoggedRoutes = ( 
+             <Routes>
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/cadastro" element={<CadPage/>} />
+           </Routes>
+    )
+
+    userRoutes = {
+
+    }
+
+
+    render(){
+        return(
+            this.userNotLoggedRoutes
+        )
+
+    }
+
+}
