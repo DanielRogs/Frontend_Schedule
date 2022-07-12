e24b2b60a2877bf78dfadce1d2fc1377a7899d46
Davi Rodrigues da Rocha <davirocha12.80@gmail.com>
2022-07-12 18:02:23 -0300

    Ajustes globalStyles

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import px2vw from "./utils/px2vw";
  
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Oxygen&display=swap');
@@ -7,9 +8,20 @@ body {
   padding: 0px;
   background-color: #F5F5F5;
   font-family: 'Oxygen', sans-serif;
+  box-sizing: border-box;
+}
+:root {
+  font-size: ${px2vw(24)};
+
+  @media (min-width: 320px) {
+    font-size: ${px2vw(18)};
+  }
+
+  @media (min-width: 768px) {
+    font-size: ${px2vw(16)};
+  }
 }
 
-
 `
- 
+
 export default GlobalStyle;
