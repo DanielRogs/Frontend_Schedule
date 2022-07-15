38195f8517bd467cb81fb75b1532a77dfe0f0af8
Gabriel <gabriel@Cartola.localdomain>
2022-07-15 08:44:47 -0300

    colocando requisição da api do lgin em services

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import {Route, Routes } from 'react-router-dom';
 import LoginPage from "../pages/LoginPage";
 import CadPage from "../pages/Cad_Page";
+import AuthContext from "../context/AuthContext";
+import { InterfaceAuthContext } from "../context/AuthContext";
 
 export default class AppRoutes extends React.Component{
 
-    
+    static contextType = AuthContext
+    declare context: React.ContextType<typeof AuthContext>
+ 
     userNotLoggedRoutes = ( 
              <Routes>
                 <Route path="/login" element={<LoginPage/>} />
@@ -13,15 +17,19 @@ export default class AppRoutes extends React.Component{
            </Routes>
     )
 
-    userRoutes = {
-
-    }
+    userRoutes = (
+             <Routes>
+                <Route path="/" element={<h1>Bem vindo</h1>} />
+           </Routes>
+    )
 
 
     render(){
-        return(
+        const {user}= this.context
 
-            this.userNotLoggedRoutes
+        return(
+            <h1>{!!user ? user.name :"vazio"}</h1>
+            
         )
 
     }
