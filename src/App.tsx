6a23155ed13ac882ad239154d92e6b5b3c4d4f10
luciano-freitas-melo <202016847@aluno.unb.br>
2022-07-17 00:00:10 -0300

    import faltando na App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppRoutes from './routes';
 import GlobalStyle from './globalStyles';
+import AuthProvider from './context/AuthProvider';
 
 
 class App extends React.Component  {
